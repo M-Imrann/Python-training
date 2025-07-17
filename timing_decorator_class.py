02974220432f2de19d6c74802142944b447a89b7
M-Imrann <m.imran146423@gmail.com>
2025-07-17 22:01:47 +0500

    Class Based Timing Decorator

---
+++ timing_decorator_class.py
@@ -0,0 +1,39 @@
+import time
+
+
+class TimingDecorator:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        start = time.time()
+        result = self.func(*args, **kwargs)
+        end = time.time()
+        print(f"Function {self.func.__name__} execution time is : "
+              f"{end - start: .5f} seconds")
+        return result
+
+
+@TimingDecorator
+def slow():
+    time.sleep(5)
+    return "Slow Function Executed."
+
+
+@TimingDecorator
+def medium():
+    time.sleep(2)
+    return "Medium Function Executed."
+
+
+@TimingDecorator
+def fast(num):
+    for i in range(num):
+        return "Fast Function Executed."
+
+
+print(slow())
+
+print(medium())
+
+print(fast(5))
